730f00cfcc3c585c399074c5159caf72702a0fd4
Rbrogniez <5013@holbertonschool.com>
2022-11-29 15:36:09 +0100

    deleting H1 for message Hello HBNB

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Hello HBNB</h1>'
+    return 'Hello HBNB'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
